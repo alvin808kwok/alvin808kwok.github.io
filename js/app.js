03cf123a413e84479218f519294b7bd389ffeef8
alvin808kwok <apple@Alvins-MacBook-Air-2.local>
2019-05-28 10:54:42 -1000

    Alvin github

--- js/app.js
+++ js/app.js
@@ -1,5 +1,71 @@
-console.log('test')
 
-//DOM
 
-document.body.innerHTML = 'Alvin is the best'
+skill1.addEventListener('click', web);
+
+function web(){
+    if(list1.style.display === 'block'){
+        list1.style.display = 'none';
+    }else{
+        list1.style.display = 'block';
+    }
+}
+
+skill2.addEventListener('click', report);
+
+function report(){
+    if(list2.style.display === 'block'){
+        list2.style.display = 'none';
+    }else{
+        list2.style.display = 'block';
+    }
+}
+
+skill3.addEventListener('click', language);
+
+function language(){
+    if(list3.style.display === 'block'){
+        list3.style.display = 'none';
+    }else{
+        list3.style.display = 'block';
+    }
+}
+
+skill4.addEventListener('click', microsoft);
+
+function microsoft(){
+    if(list4.style.display === 'block'){
+        list4.style.display = 'none';
+    }else{
+        list4.style.display = 'block';
+    }
+}
+
+skill5.addEventListener('click', google);
+
+function google(){
+    if(list5.style.display === 'block'){
+        list5.style.display = 'none';
+    }else{
+        list5.style.display = 'block';
+    }
+}
+
+skill6.addEventListener('click', tech);
+
+function tech(){
+    if(list6.style.display === 'block'){
+        list6.style.display = 'none';
+    }else{
+        list6.style.display = 'block';
+    }
+}
+
+skill7.addEventListener('click', sales);
+
+function sales(){
+    if(list7.style.display == 'block'){
+        list7.style.display = 'none';
+    }else{
+        list7.style.display = 'block';
+    }
+}
