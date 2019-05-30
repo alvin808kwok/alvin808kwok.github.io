6120e8c243ce1d93d5da5088bf3145dcd815fe1c
alvin808kwok <alvin808kwok@gmail.com>
2019-05-29 22:44:39 -1000

    alvin808kwok.github.io

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 
 
-skill1.addEventListener('click', web);
+skill1.addEventListener('click', showList1);
 
-function web(){
+function showList1(){
     if(list1.style.display === 'block'){
         list1.style.display = 'none';
     }else{
@@ -10,9 +10,9 @@ function web(){
     }
 }
 
-skill2.addEventListener('click', report);
+skill2.addEventListener('click', showList2);
 
-function report(){
+function showList2(){
     if(list2.style.display === 'block'){
         list2.style.display = 'none';
     }else{
@@ -20,9 +20,9 @@ function report(){
     }
 }
 
-skill3.addEventListener('click', language);
+skill3.addEventListener('click', showList3);
 
-function language(){
+function showList3(){
     if(list3.style.display === 'block'){
         list3.style.display = 'none';
     }else{
@@ -30,9 +30,9 @@ function language(){
     }
 }
 
-skill4.addEventListener('click', microsoft);
+skill4.addEventListener('click', showList4);
 
-function microsoft(){
+function showList4(){
     if(list4.style.display === 'block'){
         list4.style.display = 'none';
     }else{
@@ -40,9 +40,9 @@ function microsoft(){
     }
 }
 
-skill5.addEventListener('click', google);
+skill5.addEventListener('click', showList5);
 
-function google(){
+function showList5(){
     if(list5.style.display === 'block'){
         list5.style.display = 'none';
     }else{
@@ -50,9 +50,9 @@ function google(){
     }
 }
 
-skill6.addEventListener('click', tech);
+skill6.addEventListener('click', showList6);
 
-function tech(){
+function showList6(){
     if(list6.style.display === 'block'){
         list6.style.display = 'none';
     }else{
@@ -60,12 +60,92 @@ function tech(){
     }
 }
 
-skill7.addEventListener('click', sales);
+skill7.addEventListener('click', showList7);
 
-function sales(){
-    if(list7.style.display == 'block'){
+function showList7(){
+    if(list7.style.display === 'block'){
         list7.style.display = 'none';
     }else{
         list7.style.display = 'block';
+    }
+}
+
+FP.addEventListener('click', showDescription1);
+
+function showDescription1(){
+    if(description1.style.display === 'block'){
+        description1.style.display = 'none';
+    }else{
+        description1.style.display = 'block';
+    }
+}
+
+RH.addEventListener('click', showDescription2);
+
+function showDescription2(){
+    if(description2.style.display === 'block'){
+        description2.style.display = 'none';
+    }else{
+        description2.style.display = 'block';
+    }
+}
+
+FIT.addEventListener('click', showDescription3);
+
+function showDescription3(){
+    if(description3.style.display == 'block'){
+        description3.style.display = 'none';
+    }else{
+        description3.style.display = 'block';
+    }
+}
+
+WU.addEventListener('click', showDescription4);
+
+function showDescription4(){
+    if(description4.style.display === 'block'){
+        description4.style.display = 'none';
+    }else{
+        description4.style.display = 'block';
+    }
+}
+
+Puns.addEventListener('click', showDescription5);
+
+function showDescription5(){
+    if(description5.style.display === 'block'){
+        description5.style.display = 'none';
+    }else{
+        description5.style.display = 'block';
+    }
+}
+
+IHS.addEventListener('click', showDescription6);
+
+function showDescription6(){
+    if(description6.style.display === 'block'){
+        description6.style.display = 'none';
+    }else{
+        description6.style.display = 'block';
+    }
+}
+
+TCF.addEventListener('click', showDescription7);
+
+function showDescription7(){
+    if(description7.style.display === 'block'){
+        description7.style.display = 'none';
+    }else{
+        description7.style.display = 'block';
+    }
+}
+
+CDM.addEventListener('click', showDescription8);
+
+function showDescription8(){
+    if(description8.style.display === 'block'){
+        description8.style.display = 'none';
+    }else{
+        description8.style.display = 'block';
     }
 }
